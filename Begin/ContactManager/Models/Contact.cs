de4433af62ecd15aa2890f37dc2a65271cd6ba9f
MarkovicBoze <bmarkovic@etfos.hr>
2017-06-29 15:33:33 +0200

    T3 Creating the Contact Models and Augment the Contact Controller

---
+++ Begin/ContactManager/Models/Contact.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContactManager.Models
+{
+    public class Contact
+    {
+        public int Id { get; internal set; }
+        public string Name { get; internal set; }
+    }
+}
